3401971e77e06feeda605e11e5b6993eb6969765
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2022-01-21 16:24:20 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,8 @@
+echo off
+title DiscordTürkiye
+color c
+cls
+:a
+node index.js
+goto a
+pause
---
+++ index.js
@@ -0,0 +1,19 @@
+var rpc = require("discord-rpc")
+const client = new rpc.Client({ transport: 'ipc' })
+client.on('ready', () => {
+client.request('SET_ACTIVITY', {
+pid: process.pid,
+activity : {
+details : "Basic Developer For Discord.",
+assets : {
+large_image : "3",
+large_text : "Batuhan Türkoğlu#2882",
+small_image : "github",
+small_text : "Github Logo"
+},
+buttons : [{label : "Github" , url : "https://github.com/batuhantrkgl"}, {label : "Twitch" , url : "https://www.twitch.tv/batuhantrkgl"}]
+}
+})
+})
+console.log("Giriş Yapıldı!")
+client.login({ clientId : "934061876319506453" }).catch(console.error);
